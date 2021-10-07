e35385882a5ccc3070422c705e5dbc71cecfe5a4
jaspreet kaur <jaspreetkaur16june@gmail.com>
2021-10-07 21:19:12 +0530

    Left view

--- TreesLevel2/5.LeftViewOfBinaryTree.java
+++ TreesLevel2/5.LeftViewOfBinaryTree.java
@@ -1,37 +1,77 @@
-class Tree {
-    //Function to return list containing elements of left view of binary tree.
-    ArrayList < Integer > leftView(Node root) {
+import java.util.*;
 
-        ArrayList < Integer > lv = new ArrayList < > ();
+public class Main {
+    public static Scanner scn = new Scanner(System.in);
 
-        if (root == null) {
+    public static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ArrayList<Integer> leftView(TreeNode root) {
+        ArrayList<Integer> lv = new ArrayList<>();
+        
+        if(root == null){
             return lv;
         }
-
-        ArrayDeque < Node > q = new ArrayDeque < > ();
+        ArrayDeque<TreeNode>q = new ArrayDeque<>();
+        
         q.add(root);
-
-        while (q.size() > 0) {
+        while(q.size() > 0){
             int count = q.size();
-
-            lv.add(q.peek().data);
-
-            while (count-- > 0) {
+            lv.add(q.peek().val);
+            while(count-- >0){
                 //remove
-                Node rem = q.remove();
-
-                //add children
-                if (rem.left != null) {
+                TreeNode rem = q.remove();
+                
+                if(rem.left != null){
                     q.add(rem.left);
                 }
-                if (rem.right != null) {
+                if(rem.right != null){
                     q.add(rem.right);
                 }
+                
             }
-
         }
+        
+return lv;
+    }
 
-        return lv;
+    // input_section=================================================
+
+    public static TreeNode createTree(int[] arr, int[] IDX) {
+        if (IDX[0] > arr.length || arr[IDX[0]] == -1) {
+            IDX[0]++;
+            return null;
+        }
+        TreeNode node = new TreeNode(arr[IDX[0]++]);
+        node.left = createTree(arr, IDX);
+        node.right = createTree(arr, IDX);
+
+        return node;
+    }
+
+    public static void solve() {
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = scn.nextInt();
+
+        int[] IDX = new int[1];
+        TreeNode root = createTree(arr, IDX);
+
+        ArrayList<Integer> ans = leftView(root);
+        for(Integer i : ans) System.out.println(i); 
+    }
+
+    public static void main(String[] args) {
+        solve();
     }
 }
+
 
